de27b67e7242b8f88ddbc44962c68e6e52c7e65d
anrichter <anrichter2@gmail.com>
2024-08-02 23:19:54 -0500

    Filled out login and signup handlebar files. Filled out login.js and signup.js files in public folder for handling logging in and signing up for a profile from handlebar pages. Also added a formating date helper function to handle dates on the main page.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -56,7 +56,7 @@ router.get('/login', (req, res) => {
 });
 
 // Route for sign-in page
-router.get('/signin', (req, res) => {
+router.get('/signup', (req, res) => {
     if (req.session.logged_in) {
         res.redirect('/dashboard');
         return
--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const helpers = require('.utils') // might not need
+const helpers = require('./utils/date') 
 
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const hbs = exphbs.create({}) // for helpers. might not need
+const hbs = exphbs.create({ helpers }) 
 
 const sess = {
   secret: 'Super secret code',
