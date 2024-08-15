8dfdb1c7d9bed0bd732e1f7f8b53256cc8a0f9fe
anrichter <anrichter2@gmail.com>
2024-08-15 16:35:50 -0500

    Trying to get my tables seeded but running into error with the Post table.

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,10 @@
 const sequelize = require("../config/connection");
-const { User, Post } = require('../models/index');
+const { User, Post } = require('../models');
 
 const userData = require('./userSeeds.json');
 const postData = require('./postSeeds.json');
+
+console.log(postData);
 
 const seedDataBase = async () => {
     await sequelize.sync({ force: true });
@@ -12,9 +14,11 @@ const seedDataBase = async () => {
         returning: true,
     });
 
-    for (const post of postData) {
-        await Post.create({...post});
-    };
+    const posts = await Post.bulkCreate(postData)
+
+    // for (const post of postData) {
+    //     await Post.create({...post});
+    // };
 
     process.exit(0);
 };
